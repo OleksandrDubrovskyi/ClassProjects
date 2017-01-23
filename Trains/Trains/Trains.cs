b171dc8dba80abac3f57f499953d1b975fd88b0d
Oleksandr Dubrovskyi <dubrovsky@live.com>
2017-01-23 22:43:00 +0200

    Debugging and final touches

--- Trains/Trains/Trains.cs
+++ Trains/Trains/Trains.cs
@@ -15,9 +15,51 @@ namespace Trains
         const int CARS = 10;
         const int SEATS = 80;
 
-        int maxNumber = 0;
+        static bool[,,,] passengers = new bool[DAYS, TRAINS_PER_DAY, CARS, SEATS];
 
-        static bool[,,,] passengers = new bool[DAYS,TRAINS_PER_DAY,CARS,SEATS];
+
+        public static void Start()
+        {
+            Initialize(passengers);
+            ShowDayWithMaxPass();
+            ShowTrainsWithMaxPass();
+            ShowCarsWithMaxPass();
+
+        }
+
+        static void ShowCarsWithMaxPass()
+        {
+            Console.WriteLine("\nCars with max num of pass:");
+            for (int day = 0; day < DAYS; day++)
+            {
+                Console.WriteLine("Day" + day + ":");
+
+                for (int train = 0; train < TRAINS_PER_DAY; train++)
+                {
+                    Console.WriteLine("Train" + train + ":");
+                    CarWithMaximumPassengers(passengers, day, train);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void ShowTrainsWithMaxPass()
+        {
+            Console.WriteLine("\nTrains with max num of pass:");
+            for (int day = 0; day < DAYS; day++)
+            {
+                Console.WriteLine("Day" + day + ":");
+                DailyTrainWithMaximumPassengers(passengers, day);
+                Console.WriteLine();
+            }
+        }
+
+        static void ShowDayWithMaxPass()
+        {
+            Console.WriteLine("Day with maximum number of passengers:");
+            MaximumNumber max = DayWithMaximumPassengers(passengers);
+            Console.WriteLine(max.number + ": " + max.maximumPassengers);
+        }
 
         static void Initialize(bool[,,,] passengers)
         {
@@ -33,7 +75,7 @@ namespace Trains
                         {
                             passengers[day, train, car, seat] = false;
 
-                            int randomNum = rand.Next(0, 1);//Use random number generator
+                            int randomNum = rand.Next(0, 3);//Use random number generator
                             if (randomNum == 1)             //to populate trains
                             {
                                 passengers[day, train, car, seat] = true;
@@ -44,7 +86,7 @@ namespace Trains
             }
         }
 
-        static int DayWithMaximumPassengers(bool[,,,] passengers)
+        static MaximumNumber DayWithMaximumPassengers(bool[,,,] passengers)
         {
             int[] passengersPerDay = new int[DAYS];
             int counter = 0;
@@ -69,14 +111,15 @@ namespace Trains
                 }
 
                 passengersPerDay[day] = counter;
+                counter = 0;
                 max = FindMaxNumber(passengersPerDay);
                 dayWithMostPassengers = max.number;
                 maxNumOfPass = max.maximumPassengers;
             }
 
-            Console.WriteLine("Maximum number of passengers is {0} in the day # {1}",
-                              maxNumOfPass, dayWithMostPassengers);
-            return dayWithMostPassengers;
+            //Console.WriteLine("Maximum number of passengers is {0} in the day # {1}",
+            //                  maxNumOfPass, dayWithMostPassengers);
+            return max;
         }
 
         static int DailyTrainWithMaximumPassengers(bool[,,,] passengers, int day)
@@ -86,9 +129,7 @@ namespace Trains
             int trainWithMostPassengers = 0;
             int maxNumOfPass = 0;
             MaximumNumber max = new MaximumNumber();
-
-            for ( int i = day; i < day+1; day++)
-            {
+           
                 for (int train = 0; train < TRAINS_PER_DAY; train++)
                 {
                     for (int car = 0; car < CARS; car++)
@@ -102,11 +143,12 @@ namespace Trains
                         }
                     }
                     passengersPerTrain[train] = counter;
+                    counter = 0;
                     max = FindMaxNumber(passengersPerTrain);
                     trainWithMostPassengers = max.number;
                     maxNumOfPass = max.maximumPassengers;
                 }                
-            }
+            
             Console.WriteLine("Maximum number of passengers is {0} in the train # {1}",
                               maxNumOfPass, trainWithMostPassengers);
             return trainWithMostPassengers;
@@ -120,26 +162,21 @@ namespace Trains
             int maxNumOfPass = 0;
             MaximumNumber max = new MaximumNumber();
 
-            for (int i = day; i < day + 1; day++)
-            {
-                for (int j = train; j < train+1; train++)
+                for (int car = 0; car < CARS; car++)
                 {
-                    for (int car = 0; car < CARS; car++)
+                    for (int seat = 0; seat < SEATS; seat++)
                     {
-                        for (int seat = 0; seat < SEATS; seat++)
+                        if (passengers[day, train, car, seat] == true)
                         {
-                            if (passengers[day, train, car, seat] == true)
-                            {
-                                counter++;
-                            }
+                            counter++;
                         }
-                        passengersPerCar[car] = counter;
-                        max = FindMaxNumber(passengersPerCar);
-                        carWithMostPassengers = max.number;
-                        maxNumOfPass = max.maximumPassengers;
-                    }                    
+                    }
+                    passengersPerCar[car] = counter;
+                    counter = 0;
+                    max = FindMaxNumber(passengersPerCar);
+                    carWithMostPassengers = max.number;
+                    maxNumOfPass = max.maximumPassengers;
                 }
-            }
 
             Console.WriteLine("Maximum number of passengers is {0} in the car # {1}", 
                               maxNumOfPass, carWithMostPassengers);
@@ -148,7 +185,26 @@ namespace Trains
 
         static MaximumNumber FindMaxNumber(int[] passengers)
         {
-            throw new NotImplementedException();
+            MaximumNumber max = new MaximumNumber();
+            int maxNumOfPass = 0;
+            int currentMax = 0;
+            int itemWithMaximumPass = 0;
+
+            for (int i = 0; i < passengers.Length; i++)
+            {
+                currentMax = passengers[i];
+
+                if (currentMax > maxNumOfPass)
+                {
+                    maxNumOfPass = currentMax;
+                    itemWithMaximumPass = i;
+                }               
+            }
+
+            max.maximumPassengers = maxNumOfPass;
+            max.number = itemWithMaximumPass;
+
+            return max;
         }
     }
 }
